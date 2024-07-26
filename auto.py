102ce3549aa8698bbb5b81e1c899772efaa7f8c7
Alexwitk <82058380+Alexwitk@users.noreply.github.com>
2024-07-26 15:05:10 -0400

    Update auto.py
    Fixed file paths

--- auto.py
+++ auto.py
@@ -3,27 +3,32 @@ import time
 from docx2pdf import convert
 import subprocess
 import aspose.slides as slides
+from datetime import datetime
 
 # Whatever folder you want to manage downloads in
-watched_folder = r'C:\Users\Alex\Downloads'
+watched_folder = r''
+chrome_path = r''
+# Folder new files are saved to
+output_folder = r''
 seen_files = set(os.listdir(watched_folder))
-
 
 # Converts docx or pptx files to pdf's and opens in chrome browser
 def convert_to_pdf(file):
     file_extension = os.path.splitext(file)[-1].lower()
     file_path = os.path.join(watched_folder, file)
-    output_folder = r'C:\Users\Alex\Desktop\New folder (6)'
+    
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    pdf_filename = os.path.basename(file_path).replace(file_extension, f'_{timestamp}.pdf')
+    pdf_path = os.path.join(output_folder, pdf_filename)
 
     if file_extension == '.docx':
         try:
             # Convert .docx to PDF using docx2pdf
-            convert(file_path, r'C:\Users\Alex\Desktop\New folder (6)')
+            convert(file_path, output_folder)
 
             pdf_filename = os.path.basename(file_path).replace('.docx', '.pdf')
             pdf_path = os.path.join(output_folder, pdf_filename)
 
-            chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
             # Open the PDF file with Google Chrome
             subprocess.Popen([chrome_path, pdf_path], shell=True)
         except Exception as e:
@@ -33,15 +38,12 @@ def convert_to_pdf(file):
         try:
             # Converts pptx file to pdf
             with slides.Presentation(file_path) as presentation:
-                presentation.save("presentation.pdf", slides.export.SaveFormat.PDF)
+                presentation.save(pdf_path, slides.export.SaveFormat.PDF)
 
-            chrome_path = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
             # Open the PDF file with Google Chrome
-            subprocess.Popen([chrome_path, r'C:\Users\Alex\PycharmProjects\pythonProject2\presentation.pdf'],
-                             shell=True)
+            subprocess.Popen([chrome_path, pdf_path], shell=True)
         except Exception as e:
             print(f"Error converting .pptx to PDF and opening with Chrome: {e}")
-
 
 while True:
     # List all files in the watched folder
@@ -56,5 +58,5 @@ while True:
             convert_to_pdf(file)
 
     seen_files = files
-    # Timeout between checking files again
+    # Timeout between checking files againW
     time.sleep(3)
